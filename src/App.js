e09b62990e38a00927779a8d7ea451d6e9fbcc9d
Taowathar <taowathar@gmail.com>
2021-08-30 22:16:31 +0200

    Add pagination to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import ReactPaginate from "react-paginate";
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { useAxiosGet } from "./hooks/axiosGet";
-import CryptoList from "./components/CryptoList"
+import CryptoList from "./components/CryptoList";
 
 function App() {
+  let [currentPage, setCurrentPage] = useState(1);
+
+  const pageCount = 100;
+
+
+  const handlePageChange = (selectedObject) => {
+    setCurrentPage(selectedObject.selected);
+  };
 
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <div className="pagination-field">
+                <ReactPaginate
+                  pageCount={pageCount}
+                  pageRange={2}
+                  marginPagesDisplayed={2}
+                  onPageChange={handlePageChange}
+                  containerClassName={"pagination-field"}
+                  previousLinkClassName={"page"}
+                  breakClassName={"page"}
+                  nextLinkClassName={"page"}
+                  pageClassName={"page"}
+                  disabledClassNae={"disabled"}
+                  activeClassName={"active"}
+                />
+              </div>
+            </>
+          )}
+        />
+      </div>
+    </Router>
   );
 }
 
