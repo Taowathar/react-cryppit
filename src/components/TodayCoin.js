629839b5ee7fb4dae740b1a93d9b25afd057c468
Whentheknight <66561640+Whentheknight@users.noreply.github.com>
2021-09-01 12:42:50 +0200

    Add CoinDetail.js and implement its functionality to display chart for todays coin

--- src/components/TodayCoin.js
+++ src/components/TodayCoin.js
@@ -1,7 +1,7 @@
 import { useAxiosGet } from "../hooks/axiosGet";
 import Crypto from "../components/Crypto";
 import styled from "styled-components";
-import { Line } from "react-chartjs-2";
+import CoinDetail from "./CoinDetail";
 
 let currentPage = Math.floor(Math.random() * 457) + 1;
 let cryptoPerPage = 20;
@@ -10,9 +10,7 @@ let randomCurrency = Math.floor(Math.random() * 20);
 
 function TodayCoin() {
   let cryptoList = null;
-  let cryptoData = null;
   let hasCurr = false;
-  let hasData = false;
 
   const [, fetchedCryptoList] = useAxiosGet(cryptoListURL, [currentPage]);
 
@@ -22,34 +20,7 @@ function TodayCoin() {
 
   if (cryptoList != null) {
     hasCurr = true;
-    let currency = cryptoList[randomCurrency].id;
-    let dataUrl = `https://api.coingecko.com/api/v3/coins/${currency}/market_chart?vs_currency=usd&days=30&interval=daily`;
-
-    const [, fetchedCryptoData] = useAxiosGet(dataUrl, []);
-
-    if (fetchedCryptoData) {
-      cryptoData = fetchedCryptoData;
-    }
-
-    if (cryptoData != null) {
-      hasData = true;
-    }
   }
-
-  const chartState = {
-    labels: ["January", "February", "March", "April", "May"],
-    datasets: [
-      {
-        label: "Prices",
-        fill: false,
-        lineTension: 0,
-        backgroundColor: "rgba(75,192,192,1)",
-        borderColor: "rgba(0,0,0,1)",
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56],
-      },
-    ],
-  };
 
   return (
     <div>
@@ -69,6 +40,7 @@ function TodayCoin() {
               <Crypto key={crypto.id} crypto={cryptoList[randomCurrency]} />
             </tbody>
           </table>
+          <CoinDetail currency={cryptoList[randomCurrency].id}></CoinDetail>
         </div>
       )}
     </div>
