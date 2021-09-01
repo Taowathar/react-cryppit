1ab76ca3321fd79b1d9403362113b285623d0797
Taowathar <taowathar@gmail.com>
2021-09-01 09:49:56 +0200

    Get favorites from localStorage

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from "react";
-import Crypto from "./Crypto";
 
 const FavoriteList = () => {
+  let [favorites, setFavorites] = useState([]);
+  let storage = [];
+
+  useEffect(() => {
+    const getAllItemFromLocalStorage = () => {
+      let keys = Object.keys(localStorage),
+        i = keys.length;
+
+      while (i--) {
+        storage.push(JSON.parse(localStorage.getItem(keys[i])));
+      }
+
+      return storage;
+    };
+
+    setFavorites(getAllItemFromLocalStorage());
+  }, [storage]);
+
   return (
     <div className="table-container">
       <table>
