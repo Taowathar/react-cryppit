97350d5c7dd1150aa169f5766deebde4b771c16c
Taowathar <taowathar@gmail.com>
2021-09-01 13:59:02 +0200

    Create InvestButton component

---
+++ src/components/InvestButton.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const InvestButton = styled.button`
+padding: 8px;
+background-color: #0bba0b;
+color: white;
+border: 0;
+border-radius: .5rem;
+cursor: pointer;
+`
