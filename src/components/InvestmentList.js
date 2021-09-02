06630d7a79fafc134864f72165afca2f87593923
natekolozsvari <nate.kolozsvari@gmail.com>
2021-09-02 12:01:49 +0200

    Implement pagination for portfolio

--- src/components/InvestmentList.js
+++ src/components/InvestmentList.js
@@ -1,25 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import Investment from './Investment'
 
-export const InvestmentList = () => {
-    const [investmentList, setInvestmentList] = useState([]);
-
-    useEffect(() => {
-        setInvestmentList([]);
-        const portfolio = JSON.parse(localStorage.getItem('portfolio'));
-        const keys = Object.keys(portfolio);
-        keys.forEach(key => {
-            portfolio[key].id = key;
-            setInvestmentList(list => [...list, portfolio[key]])
-        })
-        investmentList.sort((a, b) => (a.price < b.price) ? 1 : -1);
-    }, [])
-    console.log(investmentList);
+export const InvestmentList = ({investments}) => {
+    
 
 
     return (
         <div>
-            {investmentList.map(crypto => (<Investment key={crypto.id} crypto={crypto}/>))}
+            {investments.map(crypto => (<Investment key={crypto.id} crypto={crypto}/>))}
         </div>
     )
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,7 @@
 import React, {useContext, useState, useEffect} from 'react'
 import { useAxiosGet } from '../hooks/axiosGet';
 import InvestmentList from './InvestmentList';
+import ReactPaginate from 'react-paginate';
 
 
 const Portfolio = () => {
@@ -14,6 +15,37 @@ const Portfolio = () => {
     const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join('%2C')}&vs_currencies=usd`;
     const [, fetchedPrices] = useAxiosGet(url, [1]);
 
+    
+    const getSortedInvestmentList = () => {
+        let investmentList = [];
+        const keys = Object.keys(portfolio);
+        keys.forEach(key => {
+            portfolio[key].id = key;
+            investmentList.push(portfolio[key])
+        })
+        investmentList.sort((a, b) => (a.price < b.price) ? 1 : -1);
+        return investmentList;
+    }
+    
+        
+    const [currentPage, setCurrentPage] = useState(0);
+    const investmentsPerPage = 2;
+    const pageCount = Math.ceil(Object.keys(portfolio).length / investmentsPerPage);
+    
+    const investmentList = getSortedInvestmentList();
+    let startIndex = (currentPage-1) * investmentsPerPage;
+    let maxIndex = startIndex + investmentsPerPage < investmentList.length ? startIndex + investmentsPerPage : investmentList.length;
+    let investmentsToDisplay = [];
+    for (let i = startIndex; i < maxIndex; i++) {
+        investmentsToDisplay.push(investmentList[i]);
+    }
+    console.log(investmentsToDisplay);
+    
+
+    const handlePageChange = (selectedObject) => {
+        setCurrentPage(selectedObject.selected + 1);
+    };
+    
 
     if (fetchedPrices) {
         Object.keys(fetchedPrices).forEach(id => {
@@ -28,7 +60,23 @@ const Portfolio = () => {
             <h2 style={{textAlign: 'center'}}>Value: ${sum.toFixed(2)} <span style={sum > totalPrice ? {color: 'green', paddingLeft: '10px'} : {color: 'red', paddingLeft: '10px'}}>{sum > totalPrice ? '+' : ''}{totalPrice === 0 ? '0.00' : ((sum/totalPrice-1)*100).toFixed(2)}%</span></h2>
             <h3 style={{textAlign: 'center', marginBottom: '5px'}}>Remaining balance: ${balance}</h3>
             <h3 style={{textAlign: 'center', marginTop: '5px'}}>Total funds: ${+(balance + sum).toFixed(2)}</h3>
-            <InvestmentList></InvestmentList>
+            <InvestmentList investments={investmentsToDisplay}></InvestmentList>
+            <div className="pagination-field">
+                      <ReactPaginate
+                        initialPage={0}
+                        pageCount={pageCount}
+                        pageRange={2}
+                        marginPagesDisplayed={2}
+                        onPageChange={handlePageChange}
+                        containerClassName={"pagination-field"}
+                        previousLinkClassName={"page"}
+                        breakClassName={"page"}
+                        nextLinkClassName={"page"}
+                        pageClassName={"page"}
+                        disabledClassNae={"disabled"}
+                        activeClassName={"active"}
+                      />
+                    </div>
         </div>
     )
 }
