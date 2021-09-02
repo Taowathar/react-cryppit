b340c859019ad8dc6ee60c1386d567c67d9e816b
natekolozsvari <nate.kolozsvari@gmail.com>
2021-09-02 10:33:29 +0200

    Create InvestmentList component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,14 @@
 
 import React, {useContext, useState, useEffect} from 'react'
 import { useAxiosGet } from '../hooks/axiosGet';
+import InvestmentList from './InvestmentList';
 
 
 const Portfolio = () => {
     const portfolio = JSON.parse(localStorage.getItem('portfolio'));
     let sum = 0;
     let totalPrice = 0;
-    const balance = localStorage.getItem('balance');
+    const balance = parseFloat(localStorage.getItem('balance'));
 
     const ids = Object.keys(portfolio);
     const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join('%2C')}&vs_currencies=usd`;
@@ -25,9 +26,11 @@ const Portfolio = () => {
     
     return (
         <div>
-            <h1 style={{textAlign: 'center'}}>Portfolio</h1>
+            <h1 style={{textAlign: 'center', fontSize: '36px'}}>Portfolio</h1>
             <h2 style={{textAlign: 'center'}}>Value: ${sum.toFixed(2)} <span style={sum > totalPrice ? {color: 'green', paddingLeft: '10px'} : {color: 'red', paddingLeft: '10px'}}>{sum > totalPrice ? '+' : ''}{totalPrice === 0 ? '0.00' : ((sum/totalPrice-1)*100).toFixed(2)}%</span></h2>
-            <h3 style={{textAlign: 'center'}}>Remaining balance: ${balance}</h3>
+            <h3 style={{textAlign: 'center', marginBottom: '5px'}}>Remaining balance: ${balance}</h3>
+            <h3 style={{textAlign: 'center', marginTop: '5px'}}>Total funds: ${balance + sum}</h3>
+            <InvestmentList></InvestmentList>
         </div>
     )
 }
