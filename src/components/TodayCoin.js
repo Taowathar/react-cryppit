6d0b31e1f84de384efaf868e8bebeea4e2e7fa82
natekolozsvari <nate.kolozsvari@gmail.com>
2021-09-02 19:42:58 +0200

    Solve daily random crypto

--- src/components/TodayCoin.js
+++ src/components/TodayCoin.js
@@ -3,18 +3,25 @@ import CoinDetail from "./CoinDetail";
 import Loading from './Loading'
 import styled from 'styled-components'
 
-
-let currentPage = Math.floor(Math.random() * 50) + 1;
-let cryptoPerPage = 20;
-let cryptoListURL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${cryptoPerPage}&page=${currentPage}&sparkline=false`;
-let randomCurrency = Math.floor(Math.random() * 20);
-
 function TodayCoin({ openModal }) {
   let cryptoList = null;
   let hasCurr = false;
   let randomCurr = null;
+  
+  // const getDailyCryptoIndex = () => {
+  //   const currentTime = Date.now();
+  //   const currentDay = Math.floor(currentTime/1000/60/60/24);
+  //   const seedrandom = require('seedrandom')
+  //   const rng = seedrandom(currentDay);
+  //   const cryptosToChooseFrom = 1000;
+  //   const dailyCryptoIndex = Math.floor(rng()*cryptosToChooseFrom) + 1;
+  //   return dailyCryptoIndex;
+  // }
 
-  const [, fetchedCryptoList] = useAxiosGet(cryptoListURL, [currentPage]);
+  const dailyCryptoIndex = getDailyCryptoIndex()
+  let cryptoListURL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=${dailyCryptoIndex}&sparkline=false`;
+
+  const [, fetchedCryptoList] = useAxiosGet(cryptoListURL, []);
 
   if (fetchedCryptoList) {
     cryptoList = fetchedCryptoList;
@@ -22,7 +29,7 @@ function TodayCoin({ openModal }) {
 
   if (cryptoList != null) {
     hasCurr = true;
-    randomCurr = cryptoList[randomCurrency];
+    randomCurr = cryptoList[0];
   }
 
   return (
@@ -57,5 +64,21 @@ const TodayDiv = styled.div`
   background-color: #f2eee3;
   margin-top: 8rem;
 `;
+
+const getDailyCryptoIndex = () => {
+  const seedrandom = require('seedrandom');
+  const cryptosToChooseFrom = 1000;
+
+  const currentUnixTime = Date.now();
+  const currentDay = milliSecondToDay(currentUnixTime);
+  
+  const rng = seedrandom(currentDay);
+  const dailyCryptoIndex = Math.floor(rng()*cryptosToChooseFrom) + 1;
+  return dailyCryptoIndex;
+}
+
+const milliSecondToDay = ms => {
+    return Math.floor(ms/1000/60/60/24);
+}
 
 export default TodayCoin;
