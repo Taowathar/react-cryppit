49ba726561a14089b1b3f2cd9cc72a22cc2df0dd
Whentheknight <66561640+Whentheknight@users.noreply.github.com>
2021-09-02 10:02:29 +0200

    Change tab icon and title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactPaginate from "react-paginate";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { useAxiosGet } from "./hooks/axiosGet";
